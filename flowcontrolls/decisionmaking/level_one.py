7a111ae877d29a958bbf3cea3e7e73eaa386736f
Helawilson <helawilson@gmail.com>
2021-01-28 11:19:28 +0530

    adding decision making programs

---
+++ flowcontrolls/decisionmaking/level_one.py
@@ -0,0 +1,6 @@
+print("you are eligible for voting")
+age=int(input("enter age"))
+if(age>=18):
+    print("u r eligibible for voting")
+else:
+    print("u r not eligible")
---
+++ flowcontrolls/decisionmaking/twonumbers.py
@@ -0,0 +1,9 @@
+num1=int(input("enter 1st  number"))
+num2=int(input("enter 2nd number"))
+if(num1+num2==100)|(num1==100)|(num2==100):
+    print("true")
+else:
+    print("false")
+
+
+
